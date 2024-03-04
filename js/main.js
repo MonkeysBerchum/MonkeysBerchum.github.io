334dea388074590140b4adbc99bef74cccd6c139
Lukas <github@notluke.de>
2024-03-04 21:48:50 +0100

    sidebar and css

--- js/main.js
+++ js/main.js
@@ -7,6 +7,15 @@ function transformX(x) {
 
 /* This function changese the display (none/block) from the menu */
 function openMenu() {
+    var custDiv = document.getElementById("sidebar");
+    if (custDiv.style.display === "block") {
+        custDiv.style.display = "none";
+    } else {
+        custDiv.style.display = "block";
+    }
+}
+
+function openMenu2() {
     var custDiv = document.getElementById("sidebar-content");
     if (custDiv.style.display === "block") {
         custDiv.style.display = "none";
